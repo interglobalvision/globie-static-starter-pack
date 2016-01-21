53dd864b4f35205e22e625acf74974918693440e
Patrick Best <patrickbest@patrickbest.com>
2016-01-21 17:38:48 -0600

    add jscsrc and tidy js file

--- js/main.js
+++ js/main.js
@@ -1,31 +1,7 @@
 /* jshint browser: true, devel: true, indent: 2, curly: true, eqeqeq: true, futurehostile: true, latedef: true, undef: true, unused: true */
-/* global $, jQuery, document, Modernizr */
-
-function l(data) {
-  'use strict';
-  console.log(data);
-}
+/* global $, document, Modernizr */
 
 $(document).ready(function () {
   'use strict';
 
-});
-
-$(window).load(function () {
-
-  /* PACKERY */
-  var pckry = new Packery( document.getElementById("gallery-container"), {
-    // options
-    itemSelector: '.photo',
-    percentPosition: true,
-    columnWidth: '.photo',
-    gutter: 0
-  });
-
-  // Display color overlay
-  $('.photo').addClass('color-overlay');
-
-  $('#gallery').animate({
-    opacity: 1
-  }, 600);
 });
